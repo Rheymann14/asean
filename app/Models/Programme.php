f8d20435bfe8286c9aca3a3e6a76ec6ee8302ded
Rheymann14 <67354994+Rheymann14@users.noreply.github.com>
2026-01-09 21:53:37 +0800

    Expand venue URL storage

--- app/Models/Programme.php
+++ app/Models/Programme.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Programme extends Model
 {
@@ -32,5 +33,10 @@ class Programme extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function venues(): HasMany
+    {
+        return $this->hasMany(Venue::class);
     }
 }
