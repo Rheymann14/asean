82ea627b9ce96a4d1048ae5ac649f9e5a23768bc
Rheymann14 <67354994+Rheymann14@users.noreply.github.com>
2026-01-11 16:09:42 +0800

    Add feedback submission endpoint

---
+++ app/Models/Feedback.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Feedback extends Model
+{
+    protected $fillable = [
+        'user_experience_rating',
+        'event_ratings',
+        'recommendations',
+    ];
+
+    protected $casts = [
+        'event_ratings' => 'array',
+    ];
+}
